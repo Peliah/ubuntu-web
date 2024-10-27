219c11cd5a1b4f0951400f8e61fe046983035991
Peliah <pelepoupa@gmail.com>
2024-10-27 22:30:48 +0100

    CHORE: Code Clean up

--- components/navbar/utils-box.tsx
+++ components/navbar/utils-box.tsx
@@ -8,7 +8,6 @@ const UtilsBox = () => {
     const router = useRouter();
     return (
         <PopoverContent className='w-96 flex flex-col space-y-4 p-4 ring-1 ring-slate-700 rounded-lg shadow-lg bg-zinc-800 text-slate-200'>
-            {/* Battery and icons section */}
             <div className='flex justify-between items-center'>
                 <div className='flex items-center space-x-2 bg-zinc-700 rounded-full p-2'>
                     <IoBatteryCharging className=' text-2xl ' />
@@ -27,7 +26,6 @@ const UtilsBox = () => {
                 </div>
             </div>
 
-            {/* Volume and brightness sliders section */}
             <div className='flex flex-col space-y-4'>
                 <div className='flex items-center space-x-2'>
                     <IoVolumeMedium className='text-xl' />
@@ -39,7 +37,6 @@ const UtilsBox = () => {
                 </div>
             </div>
 
-            {/* Additional content section */}
             <div className='flex justify-between'>
                 <span className='text-sm'>More Settings</span>
                 <span className='text-sm cursor-pointer'>View All</span>
